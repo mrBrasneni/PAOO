d4360e7770d821ba9c94738307c824b4234304a5
Brasneni Mario <mariobrasneni@gmail.com>
2022-11-24 21:08:14 +0200

    lab5

---
+++ lab5/build.sh
@@ -0,0 +1,17 @@
+# rm -rf build/;
+# mkdir build;
+
+current_dir=$PWD;
+
+cd build/;
+
+cmake ../;
+
+if cmake --build .; then 
+echo "Build succeded!";
+else 
+echo "Build failed, operation will be aborted!";
+exit
+fi
+
+./lab5
